8175c4190c76f017ba278c6d5f040834d643b922
vinzmobile1 <salesvinzmobile1@gmail.com>
2026-08-01 22:28:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
-export default defineConfig({
-  // Paksa base path menggunakan nama repositori persis
-  base: '/Web-Pricelist/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Arahkan @ ke folder src
+export default defineConfig(() => {
+  return {
+    // KUNCI PERBAIKAN: Harus persis dengan nama repo di GitHub Anda
+    base: '/Web-Pricelist/',
+    
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
-  build: {
-    outDir: 'dist',
-  }
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
 });
